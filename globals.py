d38c37e5a2fc92afc46de04af8d38d02181f7506
J4rJ4rBinks <50147212+J4rJ4rBinks@users.noreply.github.com>
2020-02-02 18:16:57 -0500

    Add files via upload

---
+++ globals.py
@@ -0,0 +1,17 @@
+import os
+import mmap
+import struct
+
+
+class globals():
+    rMin = 4
+    rMax = 23
+    gMin = 30
+    gMax = 0
+    bMin = 0
+    bMax = 60
+    Brightness = 2
+    contrast = 25
+    image_filepath = "../images/loadfile.jpg"
+    filterFile = ""
+    filterOption = 0
